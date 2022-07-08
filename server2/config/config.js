c462bc63cae88ca9b2063af7f842f5490b6f061e
asim sheikh <amazingasim3000@gmail.com>
2022-07-08 15:25:38 +0530

    added backend aswell

---
+++ server2/config/config.js
@@ -0,0 +1,14 @@
+let mongoose=require('mongoose')
+let url='mongodb+srv://AmazingAsim:AmazingAsim3000@amazingasim.tqrgz.mongodb.net/rgpv_notes?retryWrites=true&w=majority'
+
+let dbconnect=()=>{
+    return mongoose.connect(url).then((res)=>{
+        console.log("\n mongodb is connected")
+    }).catch(err=>{console.log('error in config'+err)});
+
+}
+//bla bla
+
+module.exports={dbconnect};
+
+//klfjlksdfkljsdfdf
